d3aa4a46bce514684403f8e15e43828d204bb907
aw2389 <alex2@weghorst.com>
2021-06-14 13:15:45 -0500

    13.1

--- models/index.js
+++ models/index.js
@@ -1,13 +1,3 @@
-const User = require("./User");
-const Post = require("./Post");
+const User = require('./User');
 
-//create associations (both directions needed)
-User.hasMany(Post, {
-  foreignKey: "user_id",
-});
-
-Post.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
-module.exports = { User, Post };
+module.exports = { User };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const userRoutes = require("./user-routes.js");
-const postRoutes = require("./post-routes");
+const userRoutes = require('./user-routes.js');
 
-router.use("/users", userRoutes);
-router.use("/posts", postRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
